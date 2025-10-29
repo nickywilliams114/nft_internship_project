f7564e6c17eafccb3ac956cb2f7535d4f4ad677c
Gibson <raycoutley646@gmail.com>
2025-10-29 16:53:18 -0600

    Configure gh-pages deployment and HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Explore from "./pages/Explore";
 import Author from "./pages/Author";
 import ItemDetails from "./pages/ItemDetails";
