25c650b9a34047031d9f27636165157c4b78739b
Gibson <raycoutley646@gmail.com>
2025-10-31 09:31:15 -0600

    Remove Vercel config; target GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
    
   
